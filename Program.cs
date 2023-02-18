8c5c24c0c05b152a894e0b2cd5cc9cc14d6fe56a
Daria <darya.piankova@yandex.ru>
2023-02-18 17:47:23 +0500

    init

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿int countElem(int [] elem){
+    int counter=0;
+    for(int i=0; i<elem.Length; i++){
+        if(elem[i]>0){
+            counter++;
+        }
+    }
+    return counter;
+}
+
+void PrintArr(int [] arr){
+        Console.WriteLine($"Введенный массив: [{string.Join(", ", arr)}]");    
+}
+
+Console.Clear();
+Console.Write("Введите числа: ");
+int[] elements = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+PrintArr(elements);
+Console.Write($"Результат: {countElem(elements)}");
+
+

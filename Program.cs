8d5e767931958a17ccd073e1949423a8ddf98925
Daria <darya.piankova@yandex.ru>
2023-02-18 20:02:20 +0500

    doptask2

--- Program.cs
+++ Program.cs
@@ -1,19 +1,27 @@
-﻿void CountResult(double k1, double b1, double k2, double b2){
-    double x=(b2-b1)/(k1-k2);
-    double y= k1*x+b1;
-    Console.Write($"({x};{y})");
+﻿double TriangleSquare(double x1, double y1, double x2, double y2, double x3, double y3){
+    double square= ((x2-x1)*(y3-y1)-(x3-x1)*(y2-y1))/2;
+    if(square<0)
+        return square*(-1);
+    return square;
 }
 
 Console.Clear();
-Console.Write("Введите k1: ");
-double k1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите b1: ");
-double b1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите k2: ");
-double k2 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите b2: ");
-double b2 = Convert.ToInt32(Console.ReadLine());
 
-CountResult(k1,b1,k2,b2);
+Console.Write("Введите x1: ");
+double x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите y1: ");
+double y1 = Convert.ToInt32(Console.ReadLine());
 
+Console.Write("Введите x2: ");
+double x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите y2: ");
+double y2 = Convert.ToInt32(Console.ReadLine());
 
+Console.Write("Введите x3: ");
+double x3 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите y3: ");
+double y3 = Convert.ToInt32(Console.ReadLine());
+
+double result=TriangleSquare(x1,y1,x2,y2,x3,y3);
+
+Console.Write($"Triangle's square is: {result}");
